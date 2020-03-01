5332e869e752834c9519a095a682ecb78980d942
Emik123dsa <pengrobinson@mail.ru>
2020-03-01 16:40:41 +0300

    Gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
-
+var livereload = require('gulp-livereload');
+var sass = require('gulp-sass');
+var broswerSync = require('broswer-sync');
+var minify = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
+var uglify = require('gulp-uglify'); 
+var rigger = require('gulp-rigger');
+var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
 
 gulp.task('default', function(done) 
 {
     console.log('gulp');
     done();
 });
+
